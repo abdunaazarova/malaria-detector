ed23a49c355afe29d51cf833dfebbbc383cceb06
abdunaazarova <gulnoraabdunazarova6@gmail.com>
2025-05-25 02:15:25 +0500

    initial commit - malaria detection app

---
+++ main.py
@@ -0,0 +1,95 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import io
+
+
+app = FastAPI()
+model = tf.keras.models.load_model('Malaria.h5')
+
+IMG_SIZE = 128
+app.mount('/static', StaticFiles(directory='static'),name='static')
+
+@app.get('/',response_class=HTMLResponse)
+async def root():
+    with open('static/index.html') as f:
+        return f.read()
+    
+def preprocess_image(image_bytes):
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    image = image.resize((128, 128))  
+    image = np.array(image).astype(np.float32) / 255.0  
+    image = np.expand_dims(image, axis=0)
+    return image
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = preprocess_image(image_bytes)
+    pred = float(model.predict(image)[0][0])
+    label = "Uninfected" if pred > 0.5 else "Parasitized"
+
+    return {
+        "prediction": label,
+        "confidence": round(pred * 100, 2)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
